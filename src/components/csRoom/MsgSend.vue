27e8ccbecce0d396dc00ae3011ba30d62107601d
Millie Peng <lemon@gmail.com>
2022-06-30 11:55:27 +0800

    feat:pinia render chatList & MsgSend send msg

--- src/components/csRoom/MsgSend.vue
+++ src/components/csRoom/MsgSend.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { ref } from "vue";
 import SendIcon from "@/components/svg/Send.vue";
+import { useCsRoomStore } from "@/stores/csRoom";
+const CsRoom = useCsRoomStore();
+let inputValue = ref("");
 </script>
 
 <template>
@@ -7,10 +11,15 @@ import SendIcon from "@/components/svg/Send.vue";
     class="send_section w-full h-16 flex items-center px-7 py-0 shadow-layer3Re"
   >
     <input
+      v-model="inputValue"
       type="text"
       class="w-full h-10 send_input box-border px-2 py-0 m-2.5"
     />
     <SendIcon
+      @click="
+        CsRoom.handleMsgSend(inputValue);
+        inputValue = '';
+      "
       class="cursor-pointer text-orange-Default hover:text-orange-b90 m-2.5"
     />
   </div>
